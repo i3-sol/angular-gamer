377b1848fe6cda145e27411a08c2063c5518fb06
ThunderBird0121 <spacecalian0121@gmail.com>
2022-10-22 15:56:07 +0200

    cleanup generated code

--- apps/knie-fell/src/app/app.component.ts
+++ apps/knie-fell/src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'flensrocker-root',
+  selector: 'kf-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'knie-fell';
-}
+export class AppComponent {}
