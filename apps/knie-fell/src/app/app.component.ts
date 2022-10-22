7d298437bfa5ee8c8005bd15bb2061073bda3240
ThunderBird0121 <spacecalian0121@gmail.com>
2022-10-22 16:47:45 +0200

    KnieFell: prototype upper block

--- apps/knie-fell/src/app/app.component.ts
+++ apps/knie-fell/src/app/app.component.ts
@@ -1,8 +1,13 @@
+import { ChangeDetectionStrategy, inject } from '@angular/core';
 import { Component } from '@angular/core';
+import { KnieFellStore } from './knie-fell.types';
 
 @Component({
   selector: 'kf-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  readonly #store = inject(KnieFellStore);
+}
