b2b1b4e3d7ef2bc1c6e72beb520edefbf7464631
ThunderBird0121 <spacecalian0121@gmail.com>
2022-10-29 09:56:06 +0200

    Forms: add doc comment

--- libs/forms/src/lib/raw-value-changes.ts
+++ libs/forms/src/lib/raw-value-changes.ts
@@ -9,16 +9,22 @@ export const RAW_VALUE_CHANGES_OPTIONS_DEFAULTS: RawValueChangesOptions = {
   replayCurrentValue: false,
 };
 
+/**
+ * Creates an observable that emits the raw values,
+ * whenever the valueChanges emits a value.
+ * It can optionally replay the current value
+ * when the observable is subscribed to.
+ */
 export const rawValueChanges = <TControl, TRawValue extends TControl>(
-  form: AbstractControl<TControl, TRawValue>,
-  options?: RawValueChangesOptions
+  control: AbstractControl<TControl, TRawValue>,
+  options?: Partial<RawValueChangesOptions>
 ): Observable<TRawValue> => {
   const actualOptions = { ...RAW_VALUE_CHANGES_OPTIONS_DEFAULTS, ...options };
 
   const initialTrigger$ = actualOptions.replayCurrentValue ? of(null) : EMPTY;
-  const changeTrigger$ = form.valueChanges;
+  const changeTrigger$ = control.valueChanges;
 
   return concat(initialTrigger$, changeTrigger$).pipe(
-    map(() => form.getRawValue())
+    map(() => control.getRawValue())
   );
 };
