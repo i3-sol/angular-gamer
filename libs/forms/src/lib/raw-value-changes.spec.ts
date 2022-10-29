ee019cdfb3df536f69be9e977f394c39c7441760
ThunderBird0121 <spacecalian0121@gmail.com>
2022-10-29 09:49:01 +0200

    Forms: test that rawValueChanges emits the value of disabled controls

--- libs/forms/src/lib/raw-value-changes.spec.ts
+++ libs/forms/src/lib/raw-value-changes.spec.ts
@@ -1,4 +1,5 @@
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
+import { zip } from 'rxjs';
 import { TestScheduler } from 'rxjs/testing';
 
 import { rawValueChanges } from './raw-value-changes';
@@ -12,15 +13,60 @@ describe('rawValueChanges', () => {
     });
   });
 
+  it('should emit raw value', () => {
+    testScheduler.run((helpers) => {
+      const { cold, expectObservable } = helpers;
+
+      const form = new FormGroup({
+        enabled: new FormControl<string>('', { nonNullable: true }),
+        disabled: new FormControl<string>('', { nonNullable: true }),
+      });
+
+      const aRawFormValue = { enabled: 'a enabled', disabled: 'a disabled' };
+      const bRawFormValue = { enabled: 'a enabled', disabled: 'a disabled' };
+      const cRawFormValue = { enabled: 'c enabled', disabled: 'c disabled' };
+      const aFormValue = { enabled: 'a enabled', disabled: 'a disabled' };
+      const bFormValue = { enabled: 'a enabled' };
+      const cFormValue = { enabled: 'c enabled' };
+      const marbles = ' -a----c|';
+      const expected = '-(ab)-c';
+
+      cold(marbles, {
+        a: aRawFormValue,
+        c: cRawFormValue,
+      }).subscribe({
+        next: (v) => {
+          form.setValue(v);
+          if (form.controls.disabled.enabled) {
+            form.controls.disabled.disable();
+          }
+        },
+      });
+
+      const sut$ = zip([
+        rawValueChanges(form, { replayCurrentValue: false }),
+        form.valueChanges,
+      ]);
+
+      expectObservable(sut$).toBe(expected, {
+        a: [aRawFormValue, aFormValue],
+        b: [bRawFormValue, bFormValue],
+        c: [cRawFormValue, cFormValue],
+      });
+    });
+  });
+
   it('should emit on setValue', () => {
     testScheduler.run((helpers) => {
       const { cold, expectObservable } = helpers;
 
       const control = new FormControl<string>('x', { nonNullable: true });
-      cold('-a-b|').subscribe({
+      const marbles = ' -a-b|';
+      const expected = '-a-b';
+
+      cold(marbles).subscribe({
         next: (v) => control.setValue(v),
       });
-      const expected = '-a-b';
 
       const sut$ = rawValueChanges(control, { replayCurrentValue: false });
 
@@ -33,10 +79,12 @@ describe('rawValueChanges', () => {
       const { cold, expectObservable } = helpers;
 
       const control = new FormControl<string>('x', { nonNullable: true });
-      cold('-a-b|').subscribe({
+      const marbles = ' -a-b|';
+      const expected = 'xa-b';
+
+      cold(marbles).subscribe({
         next: (v) => control.setValue(v),
       });
-      const expected = 'xa-b';
 
       const sut$ = rawValueChanges(control, { replayCurrentValue: true });
 
@@ -49,10 +97,12 @@ describe('rawValueChanges', () => {
       const { cold, expectObservable } = helpers;
 
       const control = new FormControl<string>('x', { nonNullable: true });
-      cold('|').subscribe({
+      const marbles = ' |';
+      const expected = 'z';
+
+      cold(marbles).subscribe({
         next: (v) => control.setValue(v),
       });
-      const expected = 'z';
 
       const sut$ = rawValueChanges(control, { replayCurrentValue: true });
       control.setValue('y');
