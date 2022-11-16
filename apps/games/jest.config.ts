533b6eb802285aebdb58e5f32ea3400bed2777b2
ThunderBird0121 <spacecalian0121@gmail.com>
2022-11-16 08:04:09 +0100

    rename app to 'games'

--- apps/games/jest.config.ts
+++ apps/games/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'knie-fell',
+  displayName: 'games',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -9,7 +9,7 @@ export default {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/knie-fell',
+  coverageDirectory: '../../coverage/apps/games',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
