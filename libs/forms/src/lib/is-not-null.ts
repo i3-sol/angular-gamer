d48f870f4a3a5b0dbbc435e5f9675c3370477a42
ThunderBird0121 <spacecalian0121@gmail.com>
2022-11-21 07:29:50 +0100

    isNotNull: refactor with NonNullable utility type

--- libs/forms/src/lib/is-not-null.ts
+++ libs/forms/src/lib/is-not-null.ts
@@ -1,14 +1,14 @@
 import { filter, Observable, OperatorFunction } from 'rxjs';
 
-const isNotNullGuard = <T>(r: T | null | undefined): r is T => {
-  return r != null;
+const isNotNullGuard = <T>(r: T): r is NonNullable<T> => {
+  return r !== null && r !== undefined;
 };
 
 /**
  * Typesafe filter operator which filter `null` and `undefined`.
  */
-export const isNotNull = <T>(): OperatorFunction<T | null | undefined, T> => {
-  return (source: Observable<T | null | undefined>): Observable<T> => {
+export const isNotNull = <T>(): OperatorFunction<T, NonNullable<T>> => {
+  return (source: Observable<T>): Observable<NonNullable<T>> => {
     return source.pipe(filter(isNotNullGuard));
   };
 };
